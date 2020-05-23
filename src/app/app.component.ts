c3f0c3f66c6f24141e34d43411589c5f29fac6d5
sergipereira <info@off-brand.pt>
2020-05-23 22:50:56 +0100

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,7 +17,7 @@ import { Component } from "@angular/core";
         nzTitle="Execução de Treinos | 18 de Maio, 2020"
         [nzExtra]="extraTemplate"
       >
-        <h2>Add an additional message to your Incoming Feedback widget 📣</h2>
+        <h2>Acompanha os treinos dos teus alunos em "os meus alunos" 📣</h2>
         <h3>Are your users trying to reach you through your feedback widget?</h3>
         <p>Incoming Feedback is a great way to let your users share their feedback as they navigate your site. Because the widgets are so visible, they’re often the first place your users will go when they look for help. Many of you told us that you were getting support requests via Incoming Feedback. While this works for some of you, others prefer only receiving site-related feedback this way. With our latest update we’ve got a solution for every preference.</p>
       </nz-card><br>
