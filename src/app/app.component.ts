da91499bb39dceee2545bd3aa935329ff66f314f
sergipereira <info@off-brand.pt>
2020-05-24 16:30:01 +0100

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,7 +16,8 @@ import { Component } from "@angular/core";
         style="width:auto;background-color: rgb(238, 238, 238);"
         nzTitle="Execução de Treinos | 18 de Maio, 2020"
         [nzExtra]="extraTemplate"
-      >
+        >
+        <nz-tag [nzColor]="'#2db7f5'">Novo</nz-tag>
         <h2>Acompanha os treinos dos teus alunos em "os meus alunos" 📣</h2>
         <h3>Are your users trying to reach you through your feedback widget?</h3>
         <p>Incoming Feedback is a great way to let your users share their feedback as they navigate your site. Because the widgets are so visible, they’re often the first place your users will go when they look for help. Many of you told us that you were getting support requests via Incoming Feedback. While this works for some of you, others prefer only receiving site-related feedback this way. With our latest update we’ve got a solution for every preference.</p>
@@ -27,6 +28,7 @@ import { Component } from "@angular/core";
         nzTitle="Introducing Hotjar Guides 💡 | 25 de Maio, 2020"
         [nzExtra]="extraTemplate"
       >
+        <nz-tag [nzColor]="'#faad14'">Novo</nz-tag>
         <h3>Helping you better understand user behavior, make the right changes, improve UX, and increase conversions on your website using Hotjar.</h3>
         <h5><nz-skeleton [nzActive]="true"></nz-skeleton></h5>
       </nz-card>
